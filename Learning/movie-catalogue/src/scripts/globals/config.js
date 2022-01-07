3bb1c96f388372736303f87d76094b31ccf3c59d
azarnuzy <azarnuzy@gmail.com>
2022-01-08 02:05:32 +0700

    doing request API

---
+++ Learning/movie-catalogue/src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '723efb5ac2ef3c9278d3e18eb6184aa3',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
