22f266f82c05b373e8f2daf472bbc7e96b45da73
azarnuzy <azarnuzy@gmail.com>
2022-01-09 10:23:52 +0700

    prepare operations function DB

--- Learning/movie-catalogue/src/scripts/globals/config.js
+++ Learning/movie-catalogue/src/scripts/globals/config.js
@@ -1,9 +1,12 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: '723efb5ac2ef3c9278d3e18eb6184aa3',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
   CACHE_NAME: new Date().toISOString(),
+  DATABASE_NAME: 'movie-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'movies',
 };
 
 export default CONFIG;
