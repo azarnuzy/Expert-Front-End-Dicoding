e15f7be36fec38f76727654919133c44253bcadb
azarnuzy <azarnuzy@gmail.com>
2022-01-10 08:31:01 +0700

    drawer config, URL router

--- Submission-2/src/scripts/index.js
+++ Submission-2/src/scripts/index.js
@@ -2,5 +2,18 @@ import 'regenerator-runtime';
 import '../styles/style.css';
 import '../styles/responsive.css';
 import '../styles/normalize.css';
+import App from './views/app';
 
-console.log('Hello Coders!');
+const app = new App({
+  button: document.querySelector('#menu'),
+  drawer: document.querySelector('#drawer'),
+  content: document.querySelector('#main-content'),
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', () => {
+  app.renderPage();
+});
---
+++ Submission-2/src/scripts/utils/drawer-initiator.js
@@ -0,0 +1,26 @@
+const DrawerInitiator = {
+  init({ button, drawer, content }) {
+    button.addEventListener('click', (event) => {
+      this._toggleDrawer(event, drawer);
+    });
+
+    content.addEventListener('click', (event) => {
+      this._closeDrawer(event, drawer);
+      console.log(event.target.innerHTML);
+    });
+  },
+
+  _toggleDrawer(event, drawer) {
+    event.stopPropagation();
+    drawer.classList.toggle('open');
+  },
+
+  _closeDrawer(event, drawer) {
+    event.stopPropagation();
+    if (!drawer.classList.contains('open')) {
+      drawer.classList.toggle('open');
+    }
+  },
+};
+
+export default DrawerInitiator;
