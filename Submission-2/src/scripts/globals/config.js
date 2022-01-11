dea91ed2996297755fd61a5e4a194e541fe2f378
azarnuzy <azarnuzy@gmail.com>
2022-01-11 19:37:35 +0700

    prepare operation DB function

--- Submission-2/src/scripts/globals/config.js
+++ Submission-2/src/scripts/globals/config.js
@@ -3,6 +3,9 @@ const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/large/',
   CACHE_NAME: new Date().toISOString(),
+  DATABASE_NAME: 'restro-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'restaurant',
 };
 
 export default CONFIG;
