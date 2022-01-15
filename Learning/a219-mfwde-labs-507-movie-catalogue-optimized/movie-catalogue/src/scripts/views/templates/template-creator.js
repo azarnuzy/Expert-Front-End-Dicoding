052682f2c411dff76b3bb3a0c0379544c22fe6ba
azarnuzy <azarnuzy@gmail.com>
2022-01-15 10:01:37 +0700

    initialize movie catalogue performance

---
+++ Learning/a219-mfwde-labs-507-movie-catalogue-optimized/movie-catalogue/src/scripts/views/templates/template-creator.js
@@ -0,0 +1,76 @@
+import CONFIG from '../../globals/config';
+
+const createMovieDetailTemplate = (movie) => `
+  <h2 class="movie__title">${movie.title}</h2>
+  <img class="movie__poster" src="${CONFIG.BASE_IMAGE_URL + movie.poster_path}" alt="${movie.title}" />
+  <div class="movie__info">
+  <h3>Information</h3>
+    <h4>Tagline</h4>
+    <p>${movie.tagline}</p>
+    <h4>Release Date</h4>
+    <p>${movie.release_date}</p>
+    <h4>Duration</h4>
+    <p>${movie.runtime} minutes</p>
+    <h4>Rating</h4>
+    <p>${movie.vote_average}</p>
+  </div>
+  <div class="movie__overview">
+    <h3>Overview</h3>
+    <p>${movie.overview}</p>
+  </div>
+`;
+
+const createSkeletonMovieTemplate = (count) => {
+  let template = '';
+
+  for (let i = 0; i < count; i += 1) {
+    template += `
+      <div class="movie-item">
+        <div class="movie-item__header">
+            <img class="movie-item__header__poster" width="100%" height="350px" src="./images/placeholder.png" alt="skeleton">
+        </div>
+        <div class="movie-item__content">
+            <h3 class="skeleton">Lorem ipsum dolor sit.</a></h3>
+            <p class="skeleton">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A adipisci alias aspernatur, assumenda aut consectetur consequuntur debitis deleniti dicta dolorem dolorum eos exercitationem labore laboriosam magni nihil, nobis obcaecati optio perspiciatis placeat qui recusandae saepe sapiente sequi totam ullam ut.</p>
+        </div>
+      </div>
+  `;
+  }
+  return template;
+};
+
+const createMovieItemTemplate = (movie) => `
+  <div class="movie-item">
+    <div class="movie-item__header">
+        <img class="movie-item__header__poster lazyload" width="100%" height="350px" src="./images/placeholder.png" alt="${movie.title}"
+            data-src="${movie.backdrop_path ? CONFIG.BASE_IMAGE_URL + movie.backdrop_path : 'https://picsum.photos/id/666/800/450?grayscale'}">
+        <div class="movie-item__header__rating">
+            <p>⭐️<span class="movie-item__header__rating__score">${movie.vote_average}</span></p>
+        </div>
+    </div>
+    <div class="movie-item__content">
+        <h3><a href="${`/#/detail/${movie.id}`}">${movie.title}</a></h3>
+        <p>${movie.overview}</p>
+    </div>
+  </div>
+  `;
+
+const createLikeButtonTemplate = () => `
+  <button aria-label="like this movie" id="likeButton" class="like">
+     <i class="fa fa-heart-o" aria-hidden="true"></i>
+  </button>
+`;
+
+const createLikedButtonTemplate = () => `
+  <button aria-label="unlike this movie" id="likeButton" class="like">
+    <i class="fa fa-heart" aria-hidden="true"></i>
+  </button>
+`;
+
+export {
+  createMovieItemTemplate,
+  createMovieDetailTemplate,
+  createLikeButtonTemplate,
+  createLikedButtonTemplate,
+  createSkeletonMovieTemplate,
+};
